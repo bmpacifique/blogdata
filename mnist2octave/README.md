c83d414a747bd103718cba66e9939a5a5f860ce9
Ano Nym <detzold@gmail.com>
2015-10-29 16:25:25 +0100

    update

---
+++ mnist2octave/README.md
@@ -0,0 +1,2 @@
+This is the example code for the blog post:
+[MNIST database of handwritten digits for Octave](http://daniel-e.github.io/2015-09-25-MNIST-handwritten-digits-for-Octave/)
--- mnist2octave/src/main.rs
+++ mnist2octave/src/main.rs
@@ -4,26 +4,20 @@ use std::fs::File;
 use std::io::Write;
 
 use rustml::datasets::MnistDigits;
-use rustml::csv::matrix_to_csv;
-use rustml::matrix::{Matrix, IntoMatrix};
-
-fn write(f: &mut File, m: Matrix<u8>, s: &str) {
-
-    f.write_all(format!(
-        "# name: {}\n# type: matrix\n# rows: {}\n# columns: {}\n{}\n\n",
-        s, m.rows(), m.cols(), matrix_to_csv(&m, " ")
-    ).as_bytes()).unwrap();
-}
+use rustml::io::OctaveFormat;
 
 fn main() {
 
     let (train_x, train_y) = MnistDigits::default_training_set().unwrap();
     let (test_x, test_y) = MnistDigits::default_test_set().unwrap();
 
-    let mut f = File::create("mnist.txt").unwrap();
-
-    write(&mut f, train_x, "trainX");
-    write(&mut f, train_y.to_matrix(train_y.len()), "trainY");
-    write(&mut f, test_x, "testX");
-    write(&mut f, test_y.to_matrix(test_y.len()), "testY");
+    let s = train_x.to_octave_string("trainX") +
+        &train_y.to_octave_string("trainY") +
+        &test_x.to_octave_string("testX") +
+        &test_y.to_octave_string("testY");
+    
+    File::create("mnist.txt")
+        .unwrap()
+        .write_all(s.as_bytes())
+        .unwrap();
 }
